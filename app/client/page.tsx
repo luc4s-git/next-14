7e4224b4d4496a5e5e2bc42367ed80b5e6bf7ba2
luc4s-git <lucassantos4332@hotmail.com>
2024-05-05 14:19:29 -0300

    fetch data

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 export default function Client() {
   const [count, setCount] = useState(0);
   return (
-    <div className="grid place-items-center ">
+    <div className="grid place-items-center">
       <h1 className="text-7xl font-bold">{count}</h1>
       <button className="btn" onClick={() => setCount(count + 1)}>
         +
--- app/drinks/page.tsx
+++ app/drinks/page.tsx
@@ -1,3 +1,18 @@
-export default function Drinks() {
-  return <div>Drinks</div>;
-}
+import DrinksFetch from '@/utils/interfaces/drinks';
+
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+const DrinksPage = async () => {
+  const response = await fetch(URL);
+  const data: DrinksFetch = await response.json();
+
+  return (
+    <div>
+      {data.drinks.map((item) => {
+        return <h1 key={item.idDrink}>{item.strDrink}</h1>;
+      })}
+    </div>
+  );
+};
+
+export default DrinksPage;
