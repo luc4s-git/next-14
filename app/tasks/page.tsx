5a2fdc7b67e9b9b3ce75c45ad4d9acea6685c960
luc4s-git <lucassantos4332@hotmail.com>
2024-05-14 15:56:46 -0300

    force dynamic

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,6 +1,8 @@
 import TaskForm from '@/components/tasks/TaskForm';
 import TaskList from '@/components/tasks/TaskList';
 
+export const dynamic = 'force-dynamic';
+
 export default function TasksPage() {
   return (
     <div className="m-auto max-w-96">
