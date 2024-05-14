f6093092f5deb09099b5f7ece6573c12d0fe2f48
luc4s-git <lucassantos4332@hotmail.com>
2024-05-14 14:03:49 -0300

    remove middleware redirect

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request: Request) {
-  return NextResponse.redirect(new URL('/', request.url));
+  // return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
